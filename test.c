1513cd2643162126b89f1c0cdf2448b2c504c7d8
Acsor <nildexo@yandex.com>
2018-11-10 14:26:14 +0100

    Reorder functions in `test.c'.

--- test.c
+++ test.c
@@ -7,23 +7,28 @@
 #define ERR_SIZE 256
 
 
-int test_n2t_instr_to_bitstr(void *const args, char errmsg[], size_t maxwrite);
-int test_n2t_instr_type(void *const args, char errmsg[], size_t maxwrite);
+// utils.h
 int test_n2t_strip(void *const args, char errmsg[], size_t maxwrite);
 int test_n2t_composed_of(void *const args, char errmsg[], size_t maxwrite);
 int test_n2t_decomment(void *const args, char errmsg[], size_t maxwrite);
+
+// tokenizer.h
+int test_n2t_instr_to_bitstr(void *const args, char errmsg[], size_t maxwrite);
+int test_n2t_instr_type(void *const args, char errmsg[], size_t maxwrite);
 
 typedef int (*test_function)(void*, char[], size_t);
 
 
 int main (int argc, char *argv[]) {
 	test_function tests[] = {
-		test_n2t_instr_to_bitstr, test_n2t_instr_type, test_n2t_strip,
-		test_n2t_composed_of, test_n2t_decomment
+		test_n2t_strip, test_n2t_composed_of, test_n2t_decomment,
+
+		test_n2t_instr_to_bitstr, test_n2t_instr_type
 	};
 	char *test_names[] = {
-		"test_n2t_instr_to_bitstr", "test_n2t_instr_type", "test_n2t_strip",
-		"test_n2t_composed_of", "test_n2t_decomment"
+		"test_n2t_strip", "test_n2t_composed_of", "test_n2t_decomment",
+
+		"test_n2t_instr_to_bitstr", "test_n2t_instr_type"
 	};
 	char errmsg[ERR_SIZE];
 	size_t i, tests_no = sizeof(tests) / sizeof(test_function);
@@ -41,45 +46,6 @@ int main (int argc, char *argv[]) {
 	return EXIT_SUCCESS;
 }
 
-
-int test_n2t_instr_to_bitstr(void *const args, char errmsg[], size_t maxwrite) {
-	instr inputs[] = {
-		{0}, {1}, {4}, {1 << 15}, {(1 << 15) - 1}
-	};
-	size_t const buffsize = 17;
-	char *exp_outputs[] = {
-		"0000000000000000", "0000000000000001", "0000000000000100",
-		"1000000000000000", "0111111111111111",
-	};
-	char output[buffsize];
-	size_t i, inputs_no = sizeof(inputs) / sizeof(instr);
-
-	for (i = 0; i < inputs_no; i++) {
-		n2t_instr_to_bitstr(inputs[i], output);
-
-		if (strncmp(exp_outputs[i], output, buffsize)) {
-			return 1;
-		}
-	}
-
-	return 0;
-}
-
-int test_n2t_instr_type(void *const args, char errmsg[], size_t maxwrite) {
-	instr inputs[] = {
-		{0}, {1}, {4}, {1 << 15}, {(1 << 15) - 1}
-	};
-	instr_type exp_outputs[] = {A, A, A, C, A};
-	size_t i, inputs_no = sizeof(inputs) / sizeof(instr);
-
-	for (i = 0; i < inputs_no; i++) {
-		if (exp_outputs[i] != n2t_instr_type(inputs[i])) {
-			return 1;
-		}
-	}
-
-	return 0;
-}
 
 int test_n2t_strip(void *const args, char errmsg[], size_t maxwrite) {
 	char const *inputs[] = {
@@ -143,6 +109,47 @@ int test_n2t_decomment(void *const args, char errmsg[], size_t maxwrite) {
 		}
 
 		if (strcmp(buff, exp_output[i])) {
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+int test_n2t_instr_to_bitstr(
+	void *const args, char errmsg[], size_t maxwrite
+) {
+	instr inputs[] = {
+		{0}, {1}, {4}, {1 << 15}, {(1 << 15) - 1}
+	};
+	size_t const buffsize = 17;
+	char *exp_outputs[] = {
+		"0000000000000000", "0000000000000001", "0000000000000100",
+		"1000000000000000", "0111111111111111",
+	};
+	char output[buffsize];
+	size_t i, inputs_no = sizeof(inputs) / sizeof(instr);
+
+	for (i = 0; i < inputs_no; i++) {
+		n2t_instr_to_bitstr(inputs[i], output);
+
+		if (strncmp(exp_outputs[i], output, buffsize)) {
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+int test_n2t_instr_type(void *const args, char errmsg[], size_t maxwrite) {
+	instr inputs[] = {
+		{0}, {1}, {4}, {1 << 15}, {(1 << 15) - 1}
+	};
+	instr_type exp_outputs[] = {A, A, A, C, A};
+	size_t i, inputs_no = sizeof(inputs) / sizeof(instr);
+
+	for (i = 0; i < inputs_no; i++) {
+		if (exp_outputs[i] != n2t_instr_type(inputs[i])) {
 			return 1;
 		}
 	}
