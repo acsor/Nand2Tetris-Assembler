0794d1a50020110454256fc19b6ad2af56787956
Acsor <nildexo@yandex.com>
2018-11-10 14:57:55 +0100

    Add without tests n2t_str_to_instr(), n2t_set_dest(), n2t_get_dest(), ...
    ... n2t_set_jump(), n2t_get_jump() and several other functions (even internal).

--- tokenizer.c
+++ tokenizer.c
@@ -1,4 +1,34 @@
 #include "tokenizer.h"
+#include "utils.h"
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <regex.h>
+
+
+/**
+ * Param `norm_repr': a normalized representation for the A instruction.
+ * Param `dest': `instr' variable on which to store the decoded instruction.
+ *
+ * Returns: `1' if an error occurs, `0' otherwise.
+ */
+static int n2t_str_to_ainstr(char const *norm_repr, instr *dest);
+/**
+ * Param `norm_repr': a normalized representation for the C instruction.
+ * Param `dest': `instr' variable on which to store the decoded instruction.
+ *
+ * Returns: `1' if an error occurs parsing the `dest' portion, `2' parsing the
+ * `comp' portion and `3' if parsing the `jump' portion, `0' otherwise.
+ */
+static int n2t_str_to_cinstr(char const *norm_repr, instr *dest);
+/**
+ * Parses the computation portion of a C-instruction.
+ *
+ * Param `norm_repr': a normalized representation for this ALU instruction.
+ * Returns: the integer value associated to the ALU instruction or `-1' if no
+ * correct parsing could be performed.
+ */
+static int n2t_parse_cinstr_comp(char const *norm_repr);
 
 
 int n2t_instr_to_bitstr(instr const in, char *const dest) {
@@ -16,6 +46,49 @@ int n2t_instr_to_bitstr(instr const in, char *const dest) {
 	return i + 1;
 }
 
+int n2t_str_to_instr(char const *str_repr, instr *dest) {
+	char normalized[strlen(str_repr) + 1];
+
+	if (n2t_strip(str_repr, normalized)) {
+		return 1;
+	}
+
+	if (normalized[0] == '@') {
+		return n2t_str_to_ainstr(normalized, dest);
+	} else {
+		// Prototypical regex: "[ADM]\s*=\s*" for the assignment part.
+		return n2t_str_to_cinstr(normalized, dest);
+	}
+}
+
+int n2t_set_dest(instr *dest, int dest_reg) {
+	if (0 <= dest_reg && dest_reg < 8) {
+		dest->bits |= dest_reg << 3;
+
+		return 0;
+	}
+
+	return 1;
+}
+
+int n2t_get_dest(instr *dest) {
+	return (dest->bits && (7 << 3)) >> 3;
+}
+
+int n2t_set_jump(instr *dest, int jump_cond) {
+	if (jump_cond <= 0 && jump_cond <= 7) {
+		dest->bits |= jump_cond;
+
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
+int n2t_get_jump(instr *dest) {
+	return dest->bits & 7;
+}
+
 instr_type n2t_instr_type(instr const in) {
 	// 0x8000 = 1 << 15 = 1000 0000 0000 0000 (binary form).
 	if (in.bits & 0x8000) {
@@ -23,4 +96,189 @@ instr_type n2t_instr_type(instr const in) {
 	}
 
 	return A;
+}
+
+static int n2t_str_to_ainstr(char const *norm_repr, instr *dest) {
+	if (norm_repr[0] != '@') {
+		return 1;	// Not an A-instruction.
+	}
+
+	if (n2t_is_numeric(norm_repr + 1) && norm_repr[1] != '0') {
+		// `[1-9]\d*' digits.
+		dest->bits = atoi(norm_repr + 1);
+		strncpy(dest->text_repr, norm_repr, TEXT_REPR_SIZE);
+		dest->loaded = 1;
+	} else if (	// @R0, @R1, ..., @R15
+			norm_repr[1] == 'R' && n2t_is_numeric(norm_repr + 2) &&\
+			atoi(norm_repr + 2) < 16
+			) {
+		dest->bits = atoi(norm_repr + 2);
+		strncpy(dest->text_repr, norm_repr, TEXT_REPR_SIZE);
+		dest->loaded = 1;
+	} else if (n2t_is_alpha(norm_repr + 1, "_")) {		// @LABEL, @label
+		strncpy(dest->text_repr, norm_repr, TEXT_REPR_SIZE);
+		dest->loaded = 0;
+	} else {
+		return 1;
+	}
+
+	return 0;
+}
+
+static int n2t_str_to_cinstr(char const *norm_repr, instr *dest) {
+	char const
+		*dest_field = index(norm_repr, '='),
+		*jump_field = index(norm_repr, ';');
+
+	char parsed_dest[3] = "   ";
+	size_t parsed_dest_index = 0;
+
+	char *dbuff;
+	size_t dbuff_size;
+
+	char *comp_field;
+	int comp_encoding;
+
+	memset((void*)dest, 0, sizeof(instr));
+
+	// Parse the `comp' part.
+	// If '=' was not found in `norm_repr', `dest_field' will be `NULL'.
+	while (norm_repr < dest_field) {
+		if (!IS_SPACE(*norm_repr)) {
+			if (index(parsed_dest, *norm_repr)) {
+				return 1;	// We already parsed `*norm_repr'!
+			}
+
+			switch (*norm_repr) {
+				case 'M':
+					n2t_set_dest(dest, DEST_M);
+					parsed_dest[parsed_dest_index] = 'M';
+					break;
+				case 'D':
+					n2t_set_dest(dest, DEST_D);
+					parsed_dest[parsed_dest_index] = 'D';
+					break;
+				case 'A':
+					n2t_set_dest(dest, DEST_A);
+					parsed_dest[parsed_dest_index] = 'A';
+					break;
+				default:
+					return 1;
+			}
+		}
+
+		norm_repr++;
+	}
+
+	// Parse the `jump' part.
+	if (jump_field) {
+		dbuff_size = strlen(jump_field);
+		dbuff = (char*) malloc(dbuff_size);
+		strncpy(dbuff, jump_field + 1, dbuff_size);
+		n2t_strip(dbuff, dbuff);
+
+		if (!strncmp(dbuff, "JGT", dbuff_size)) {
+			n2t_set_jump(dest, JUMP_GT);
+		} else if (!strncmp(dbuff, "JEQ", dbuff_size)) {
+			n2t_set_jump(dest, JUMP_EQ);
+		} else if (!strncmp(dbuff, "JGE", dbuff_size)) {
+			n2t_set_jump(dest, JUMP_GE);
+		} else if (!strncmp(dbuff, "JLT", dbuff_size)) {
+			n2t_set_jump(dest, JUMP_LT);
+		} else if (!strncmp(dbuff, "JNE", dbuff_size)) {
+			n2t_set_jump(dest, JUMP_NE);
+		} else if (!strncmp(dbuff, "JLE", dbuff_size)) {
+			n2t_set_jump(dest, JUMP_LE);
+		} else if (!strncmp(dbuff, "JMP", dbuff_size)) {
+			n2t_set_jump(dest, JUMP_ALWAYS);
+		} else {
+			return 3;
+		}
+
+		free(dbuff);
+		dbuff_size = 0;
+	}
+
+	// Parse the `comp' part.
+	comp_field = dest_field ? strdup(dest_field): strdup(norm_repr);
+	if (jump_field) {
+		comp_field[
+			(index(comp_field, ';') - comp_field) / sizeof(comp_field)
+		] = '\0';
+	}
+	n2t_strip(comp_field, comp_field);
+	n2t_collapse_any(comp_field, " \t", comp_field);
+
+	if ((comp_encoding = n2t_parse_cinstr_comp(comp_field)) > -1) {
+		dest->bits |= comp_encoding << 6;
+	} else {
+		return 2;
+	}
+
+	free(comp_field);
+	dest->bits |= (7 << 13);
+
+	return 0;
+}
+
+static int n2t_parse_cinstr_comp(char const *norm_repr) {
+	if (!strcmp(norm_repr, "0")) {
+		return 32 + 8 + 2;
+	} else if (!strcmp(norm_repr, "1")) {
+		return 63;
+	} else if (!strcmp(norm_repr, "-1")) {
+		return 32 + 16 + 8 + 2;
+	} else if (!strcmp(norm_repr, "D")) {
+		return 8 + 4;
+	} else if (!strcmp(norm_repr, "A")) {
+		return 32 + 16;
+	} else if (!strcmp(norm_repr, "!D")) {
+		return 8 + 4 + 1;
+	} else if (!strcmp(norm_repr, "!A")) {
+		return 32 + 16 + 1;
+	} else if (!strcmp(norm_repr, "-D")) {
+		return 8 + 4 + 2 + 1;
+	} else if (!strcmp(norm_repr, "-A")) {
+		return 32 + 16 + 2 + 1;
+	} else if (!strcmp(norm_repr, "D+1")) {
+		return 16 + 8 + 4 + 2 + 1;
+	} else if (!strcmp(norm_repr, "A+1")) {
+		return 32 + 16 + 4 + 2 + 1;
+	} else if (!strcmp(norm_repr, "D-1")) {
+		return 8 + 4 + 2;
+	} else if (!strcmp(norm_repr, "A-1")) {
+		return 32 + 16 + 2;
+	} else if (!strcmp(norm_repr, "D+A")) {
+		return 2;
+	} else if (!strcmp(norm_repr, "D-A")) {
+		return 16 + 2 + 1;
+	} else if (!strcmp(norm_repr, "A-D")) {
+		return 4 + 2 + 1;
+	} else if (!strcmp(norm_repr, "D&A")) {
+		return 0;
+	} else if (!strcmp(norm_repr, "D|A")) {
+		return 16 + 4 + 1;
+	} else if (!strcmp(norm_repr, "M")) {
+		return 64 + 32 + 16;
+	} else if (!strcmp(norm_repr, "!M")) {
+		return 64 + 32 + 16 + 1;
+	} else if (!strcmp(norm_repr, "-M")) {
+		return 64 + 32 + 16 + 2 + 1;
+	} else if (!strcmp(norm_repr, "M+1")) {
+		return 64 + 32 + 16 + 8 + 4 + 2;
+	} else if (!strcmp(norm_repr, "M-1")) {
+		return 64 + 32 + 16 + 2;
+	} else if (!strcmp(norm_repr, "D+M")) {
+		return 2;
+	} else if (!strcmp(norm_repr, "D-M")) {
+		return 16 + 2 + 1;
+	} else if (!strcmp(norm_repr, "M-D")) {
+		return 4 + 2 + 1;
+	} else if (!strcmp(norm_repr, "D&M")) {
+		return 64;
+	} else if (!strcmp(norm_repr, "D|M")) {
+		return 64 + 16 + 4 + 1;
+	}
+
+	return -1;
 }
--- tokenizer.h
+++ tokenizer.h
@@ -2,9 +2,40 @@
 #define	TOKENIZER_H
 
 
+#define SYM_EQ		'='
+#define SYM_SEMIC	';'
+
+#define	DEST_NONE 0
+#define	DEST_M 1
+#define	DEST_D 2
+#define	DEST_MD 3
+#define	DEST_A 4
+#define	DEST_AM 5
+#define	DEST_AD 6
+#define	DEST_AMD 7
+
+#define JUMP_NONE 0
+#define JUMP_GT 1
+#define JUMP_EQ 2
+#define JUMP_GE 3
+#define JUMP_LT 4
+#define JUMP_NE 5
+#define JUMP_LE 6
+#define JUMP_ALWAYS 7
+
+#define	TEXT_REPR_SIZE 64
+
+
 // TO-DO Employ a data type which has a width of two bytes cross-platform.
 typedef struct {
 	unsigned short int bits;
+	// Human-readable version of the instruction (e.g., as may be read from a
+	// source file).
+	char text_repr[TEXT_REPR_SIZE];
+	// Tokenizer hint for whether this struct instance has been initialized
+	// during the first parsing phase, or should be filled in later. (Mostly
+	// useful for A-instructions.)
+	short int loaded;
 } instr;
 
 typedef enum {
@@ -21,6 +52,48 @@ typedef enum {
  * occurred.
  */
 int n2t_instr_to_bitstr(instr const in, char *const dest);
+/**
+ * Instantiates an `instr' structure from `str_repr', containing its
+ * human-readable textual representation.
+ *
+ * Returns: `1' if `str_repr' could not be converted into an `instr' type, `0'
+ * otherwise.
+ */
+int n2t_str_to_instr(char const *str_repr, instr *dest);
+/**
+ * Sets the destination register(s) of a C-instruction computation (to choose
+ * between `A', `D' or `M').
+ *
+ * Param `dest_reg': can assume either `DEST_A', `DEST_D' or `DEST_M' values,
+ * or an or-ed expression amongst them.
+ *
+ * Returns: `1' if invalid arguments were supplied, `0' otherwise.
+ */
+// TO-DO Test
+int n2t_set_dest(instr *dest, int dest_reg);
+/**
+ * Retrieves the destinationn register(s) of a C-instruction computation.
+ *
+ * Returns: `DEST_M', `DEST_D', `DEST_MD', `DEST_A', ..., comprising any
+ * possible combination of the three register.
+ */
+// TO-DO Test
+int n2t_get_dest(instr *dest);
+/**
+ * Sets the jump condition of a C-instruction computation (to choose between
+ * `JUMP_NONE' or `JUMP_GT', `JUMP_EQ', `JUMP_LT' or any or-ed expression
+ * amongst them.
+ *
+ * Returns: `1' if invalid arguments were supplied, `0' otherwise.
+ */
+// TO-DO Test
+int n2t_set_jump(instr *dest, int jump_cond);
+/**
+ * Returns: `JUMP_NONE', `JUMP_GT', `JUMP_EQ', `JUMP_GE', ..., or any other
+ * combination up to `JUMP_ALWAYS'.
+ */
+// TO-DO Test
+int n2t_get_jump(instr *dest);
 /**
  * Returns: the instruction type associated with `in'.
  */
